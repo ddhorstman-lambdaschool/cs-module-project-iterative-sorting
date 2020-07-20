227bfb8f47e8a1b360c3d1e4cf7bfd2a5fec6f1a
David Horstman <davidzilla27@gmail.com>
2020-07-20 12:54:17 -0400

    Bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,8 +17,13 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    swapped_this_cycle = True
+    while swapped_this_cycle:
+        swapped_this_cycle = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped_this_cycle = True
 
     return arr
 
